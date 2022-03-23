0dd073683d5676f42a091474221b3a69826937b3
Mcouple <suplzy0409@gmail.com>
2022-03-23 17:19:15 +0800

    delete

--- love/get.js
+++
@@ -1,1 +0,0 @@
-var xhr=new XMLHttpRequest();xhr.open("get","https://v1.hitokoto.cn");xhr.onreadystatechange=function(){if(xhr.readyState===4){var data=JSON.parse(xhr.responseText);var hitokoto=document.getElementById("hitokoto");hitokoto.innerText=data.hitokoto}};xhr.send();
